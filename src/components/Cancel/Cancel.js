a8f2037a93ccd3377799007af62210046867bc9e
vyomshah05 <75156655+vyomshah05@users.noreply.github.com>
2021-11-11 21:18:31 -0800

    Update Cancel.js

--- src/components/Cancel/Cancel.js
+++ src/components/Cancel/Cancel.js
@@ -6,7 +6,35 @@ import './style.css';
 const Cancel = () => {
     return (
         <div>
-            cancel
+            <body data-new-gr-c-s-check-loaded="14.1038.0" data-gr-ext-installed="" class="vsc-initialized" data-new-gr-c-s-loaded="14.1038.0">
+<h1 style="text-align: center;"><span style="font-family:arial,helvetica,sans-serif;"><img alt="" src="https://fremontptsa.org/files/fhslogo.png" style="width: 300px; height: 100px; float: left;"></span></h1>
+
+<h1 style="text-align: center;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<img alt="" src="https://fremontptsa.org/files/logoptsa.jpg" style="width: 190px; height: 60px;"></h1>
+
+<h1 style="text-align: center;"></h1>
+
+<h1><span style="font-family:arial,helvetica,sans-serif;">Your transaction is now canceled!</span></h1>
+
+<p></p>
+
+<p></p>
+
+<p><span style="font-size:28px;"><span style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 15.4821px; text-align: center; background-color: rgb(250, 251, 252);">You can now browse back to our <a href="https://fremontptsa.org">donation page</a>&nbsp;</span></span></p>
+
+<p></p>
+
+<p></p>
+
+<p></p>
+
+<p><span style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 15.4821px; text-align: center; font-size: 28px; background-color: rgb(250, 251, 252);">or close this window.</span></p>
+
+<p></p>
+
+<p></p>
+
+
+</body>
         </div>
     )
 }
