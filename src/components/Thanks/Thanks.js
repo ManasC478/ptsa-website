37b02755dfa74ec1a30a869cff53a07ae7ac6e15
rahulravin11 <94147720+rahulravin11@users.noreply.github.com>
2021-11-11 21:19:41 -0800

    Update Thanks.js

--- src/components/Thanks/Thanks.js
+++ src/components/Thanks/Thanks.js
@@ -6,7 +6,35 @@ import './style.css';
 const Thanks = () => {
     return (
         <div>
-            thanks
+            <html><head><meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
+	
+	<title>FHS PTSA Donation Page</title>
+</head>
+<body class="vsc-initialized" data-new-gr-c-s-check-loaded="14.1038.0" data-gr-ext-installed="">
+<h1><span style="font-family: arial, helvetica, sans-serif;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span><span style="font-family: arial, helvetica, sans-serif;">&nbsp;&nbsp; &nbsp;</span><img alt="" src="https://fremontptsa.org/files/logoptsa.jpg" style="font-family: arial, helvetica, sans-serif; width: 190px; height: 60px;"><span style="font-family:arial,helvetica,sans-serif;"><img alt="" src="https://fremontptsa.org/files/fhslogo.png" style="width: 300px; height: 100px; float: left;"></span></h1>
+
+<h1 style="text-align: center;"></h1>
+
+<h1><span style="font-family: arial, helvetica, sans-serif;">Thank you for your donation!</span></h1>
+
+<p><span style="font-size:28px;"><span style="font-family:arial,helvetica,sans-serif;">You will soon receive an email Confirmation/Receipt from Paypal. Your transaction will show up in your credit Card statement either as "</span><span style="font-family: arial, helvetica, sans-serif; background-color: rgb(255, 255, 255);">PP*FREMONTHIGH PTSA" or as "PAYPAL *FHS PTSA"</span></span></p>
+
+<p></p>
+
+<p><span style="font-size:28px;"><span style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;  text-align: center; background-color: rgb(250, 251, 252);">Fremont High PTSA is a 501 (c)(3) tax-exempt non profit organization and your gift may qualify as a charitable deduction for federal income tax purposes.&nbsp;Please consult with your&nbsp;legal or tax advisor.</span></span></p>
+
+<p><span style="font-size:28px;"><span style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; text-align: center; background-color: rgb(250, 251, 252);">IRS EIN: 94-2852575</span></span></p>
+
+<p><span style="font-size:28px;"><span style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;  text-align: center; background-color: rgb(250, 251, 252);">Your transaction is complete. &nbsp;</span></span></p>
+
+<p><span style="font-size:28px;"><span style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;  text-align: center; background-color: rgb(250, 251, 252);">You can now browse to <a href="https://fhs.fuhsd.org">Fremont High School Website</a> or close this window.</span></span></p>
+
+<p></p>
+
+<p></p>
+
+
+</body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>
         </div>
     )
 }
