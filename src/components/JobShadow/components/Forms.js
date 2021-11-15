b3f2da7c944ac07356971efb0474ad343922e81e
ManasC478 <manas.chougule@gmail.com>
2021-11-14 23:46:23 -0800

    updated forms page

--- src/components/JobShadow/components/Forms.js
+++ src/components/JobShadow/components/Forms.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Forms = () => {
     return (
         <section className="forms">
-            <p>Liability Waiver, Medical Authorization, and additional forms required to be filled before the Job Shadow Day.</p>
+            <p>Medical Authorization and Liability Waiver:</p>
+            <br />
+            <p>Students who are confirmed for Job Shadow Day are required (mandatory) to complete the below form in full prior to attending the Job Shadow Day and email it to <a href="mailto:fhsptsajobshadow@gmail.com">fhsptsajobshadow@gmail.com</a>.</p>
+            <br />
+            <p>Note that there might be additional or similar forms from the Host needs to completed and it varies from Host to Host. We will share the Host required forms once we finalize the Host list.</p>
+            <br />
+            <a href="https://docs.google.com/document/d/1UjmGtB_TvGj8_BYHYjeRWSCS_W7zzPMy/edit" rel="noreferrer" target="_blank">Medical Auth and Liability waiver</a>
         </section>
     )
 }
